98c2f15bcf8f13eb84f2f828064b657d1d24b172
huyqng1412 <huyqng1412@gmail.com>
2025-11-03 14:35:48 -0500

    finished employee and project data

--- database/insert_employee_data.sql
+++ database/insert_employee_data.sql
@@ -22,6 +22,7 @@ INSERT INTO `project` (`project_name`, `start_date`, `project_dscr`)
 VALUES
     ('Talenti Products Marketing Event', '2025-10-03', 'Creates and distributes an online survey to gather and analyze customer feedback on various Talenti ice cream flavors for a marketing campaign.'),
     ('Stair-Climbing Robot', '2025-08-18', 'Build a robot that can recognize stairs as it approaches them and find a method to climb them. Each attempt to identify stairs will be recorded in a database to help train the robot.'),
-    ('Nutrition AI Chatbot', '2025-01-01', 'Create a personalized nutrition AI chatbot to answer questions on food, drugs, medicines, and nutrition facts. Chat data will train the AI to better meet individual user needs.'),
-    ('Vietnamese Pub Sales System', '2025-02-20', 'Develop a sales system for a Vietnamese pub restaurant to summarize daily sales, collect customer data, and generate sales insights.')
+    ('Nutrition AI Chatbot', '2025-01-01', 'Create a personalized nutrition AI chatbot to answer questions on food, drugs, medicines, and nutrition facts. Chat data will train the AI to meet individual user needs better.'),
+    ('Vietnamese Pub Sales System', '2025-02-20', 'Develop a sales system for a Vietnamese pub restaurant to summarize daily sales, collect customer data, and generate sales insights.'),
+    ('Mechanic Logging System', '2025-07-15', 'Develop a tracking and logging system for mechanical engineers to manage daily workflows and outstanding tasks. Collaboration with a mechanical engineer is required to understand device algorithms for system optimization.')
     ;
