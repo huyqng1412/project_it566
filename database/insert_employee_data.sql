74157b545880e38aa1e0eb9118c58576f5e164e4
huyqng1412 <huyqng1412@gmail.com>
2025-11-04 16:43:14 -0500

    Added project total hours and total fte

--- database/insert_employee_data.sql
+++ database/insert_employee_data.sql
@@ -18,11 +18,11 @@ VALUES
     ('Bobby', 'Dang', '2000-09-10', 'M')
     ;
 
-INSERT INTO `project` (`project_name`, `start_date`, `total_fte`, `project_dscr`)
+INSERT INTO `project` (`project_name`, `start_date`, `total_hours`, `total_fte`, `project_dscr`)
 VALUES
-    ('Talenti Marketing Event', '2025-10-03', 'Creates and distributes an online survey to gather and analyze customer feedback on various Talenti ice cream flavors for a marketing campaign.'),
-    ('Stair-Climbing Robot', '2025-08-18', 'Build a robot that can recognize stairs as it approaches them and find a method to climb them. Each attempt to identify stairs will be recorded in a database to help train the robot.'),
-    ('Nutrition AI Chatbot', '2025-01-01', 'Create a personalized nutrition AI chatbot to answer questions on food, drugs, medicines, and nutrition facts. Chat data will train the AI to meet individual user needs better.'),
-    ('Pub Sales System', '2025-02-20', 'Develop a sales system for a Vietnamese pub restaurant to summarize daily sales, collect customer data, and generate sales insights.'),
-    ('Mechanic Logging System', '2025-07-15', 'Develop a tracking and logging system for mechanical engineers to manage daily workflows and outstanding tasks. Collaboration with a mechanical engineer is required to understand device algorithms for system optimization.')
+    ('Talenti Marketing Event', '2025-10-03', '320', '2', 'Creates and distributes an online survey to gather and analyze customer feedback on various Talenti ice cream flavors for a marketing campaign.'),
+    ('Stair-Climbing Robot', '2025-08-18', '480', '3', 'Build a robot that can recognize stairs as it approaches them and find a method to climb them. Each attempt to identify stairs will be recorded in a database to help train the robot.'),
+    ('Nutrition AI Chatbot', '2025-01-01', '240', '1.5', 'Create a personalized nutrition AI chatbot to answer questions on food, drugs, medicines, and nutrition facts. Chat data will train the AI to meet individual user needs better.'),
+    ('Pub Sales System', '2025-02-20', '160', '1', 'Develop a sales system for a Vietnamese pub restaurant to summarize daily sales, collect customer data, and generate sales insights.'),
+    ('Mechanic Logging System', '2025-07-15', '200', '1.25', 'Develop a tracking and logging system for mechanical engineers to manage daily workflows and outstanding tasks. Collaboration with a mechanical engineer is required to understand device algorithms for system optimization.')
     ;
