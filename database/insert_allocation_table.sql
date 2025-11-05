5a647a2c5f35bc3f53bb2271a7510926878034eb
huyqng1412 <huyqng1412@gmail.com>
2025-11-05 11:16:43 -0500

    Add data into project_allocations table

--- database/insert_allocation_table.sql
+++ database/insert_allocation_table.sql
@@ -1,5 +1,33 @@
+/* Insert data into the 'project_management' database. */
+
+/* Switch to the project_management database. */
 USE `project_management`
 
-INSERT INTO `project_allocations` (`project_id`, `employee_id`, `assigned_fte`)
+/* Insert data into the project_allocations table */
+INSERT INTO `project_allocations` (`employee_id`, `project_id`, `assigned_fte`)
 VALUES
-    ()
+    ('8', '1000', '0.5'),
+    ('7', '1000', '0.5'),
+    ('1', '1000', '0.5'),
+    ('9', '1000', '0.5'),
+
+    ('2', '1001', '1'),
+    ('3', '1001', '0.5'),
+    ('4', '1001', '0.5'),
+    ('1', '1001', '1'),
+
+    ('5', '1002', '0.5'),
+    ('1', '1002', '0.25'),
+    ('7', '1002', '0.25'),
+    ('6', '1002', '0.25'),
+
+    ('3', '1002', '0.25'),
+    ('6', '1003', '0.5'),
+    ('1', '1003', '0.15'), 
+    ('8', '1003', '0.15'),
+    ('9', '1003', '0.2'),
+
+    ('4', '1004', '0.5'),
+    ('2', '1004', '0.25'),
+    ('3', '1004', '0.25'),
+    ('1', '1004', '0.25')
