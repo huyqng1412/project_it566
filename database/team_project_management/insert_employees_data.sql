5fd6a90caa4b1991394459633b29da65b31e3949
huyqng1412 <huyqng1412@gmail.com>
2025-11-10 13:30:29 -0500

    new database following ch24

---
+++ database/team_project_management/insert_employees_data.sql
@@ -0,0 +1,16 @@
+USE `team_project_management`
+
+
+
+INSERT INTO `employees` (`first_name`, `last_name`, `birthday`, `gender`)
+VALUES
+    ('Huy', 'Nguyen', '2000-12-14', 'M'),
+    ('Long', 'Tran', '1999-02-19', 'M' ),
+    ('Sinh', 'Tran', '2001-05-20', 'M'),
+    ('Hoang', 'Nguyen', '2001-03-13', 'M'),
+    ('Khang', 'Bui', '2000-12-19', 'M'),
+    ('Hieu', 'Chau', '1996-02-15', 'M'),
+    ('Phuong', 'Huynh', '2004-11-18', 'M'),
+    ('Han', 'Tran', '2003-03-13', 'F'),
+    ('Bobby', 'Dang', '2000-09-10', 'M')
+    ;
---
+++ database/team_project_management/insert_projects_data.sql
@@ -0,0 +1,11 @@
+USE `team_project_management`
+
+
+INSERT INTO `projects` (`title`, `total_hours`, `total_fte`, `status`)
+VALUES
+    ('Talenti Marketing Event', '320', '2', 'Done'),
+    ('Stair-Climbing Robot', '480', '3', 'In Progress'),
+    ('Nutrition AI Chatbot', '240', '1.5', 'In Progress'),
+    ('Pub Sales System', '160', '1', 'Done'),
+    ('Mechanic Logging System', '200', '1.25', 'In Progress')
+    ;
